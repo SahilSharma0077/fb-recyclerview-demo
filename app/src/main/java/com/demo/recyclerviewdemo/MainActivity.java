edbd8b048c8db9b51cee2249b6d829e9a91a8555
SahilSharma0077 <sahilibyte@gmail.com>
2021-05-31 20:03:41 +0530

    test commit

--- app/src/main/java/com/demo/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/demo/recyclerviewdemo/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //some error came on code part
         setupViews();
     }
 
